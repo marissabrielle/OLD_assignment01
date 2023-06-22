e38bff38ada4232d8933a2f634197ec198fe36e1
marissabrielle <marissabrielle@gmail.com>
2023-06-21 21:35:29 -0400

    steps 1-5 completed

--- js/script.js
+++ js/script.js
@@ -1,7 +1,46 @@
 // STEP 1 
-
+// let someMonth; 
+// function theMonth(); 
+// let currentMonth; 
+// let summerMonth; 
+// let MyLibraryFunction
 
 // STEP 2 
+// numeric literal expression
+// 4
+
+// string literal expression 
+// "Hello world!"
+
+// Boolean literal expression 
+// true
+
+// null literal expression
+// null
 
 
 // STEP 3
+let myFirstExpression = (6 / 3) * 4 + 6;
+let anotherExpression = 3 + (8 / 4) + 7;
+
+// STEP 4
+let firstName;
+let strLastName;
+let address;
+let city;
+let state;
+let iZipCode;
+let yourAge;
+let strReferralSource;
+let mayWeContactYou;
+
+// STEP 5
+address = '1234 Avenue Way';
+city = 'San Diego';
+state = '1234 Avenue Way';
+
+let address = '1234 Avenue Way';
+let city = 'San Diego';
+let state =' 1234 Avenue Way';
+
+let address = '1234 Avenue Way', city = 'San Diego', state =' 1234 Avenue Way';
