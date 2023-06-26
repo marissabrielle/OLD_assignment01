073d0b78c60da9da6742eae104af88203db8a423
marissabrielle <marissabrielle@gmail.com>
2023-06-25 21:08:13 -0400

    finished steps 1-22

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@
 // let summerMonth; 
 // let MyLibraryFunction
 
+
 // STEP 2 
 // numeric literal expression
 // 4
@@ -18,29 +19,137 @@
 // null literal expression
 // null
 
-
 // STEP 3
-let myFirstExpression = (6 / 3) * 4 + 6;
-let anotherExpression = 3 + (8 / 4) + 7;
+// let myFirstExpression = (6 / 3) * 4 + 6;
+// let anotherExpression = 3 + (8 / 4) + 7;
+
 
 // STEP 4
-let firstName;
-let strLastName;
-let address;
-let city;
-let state;
-let iZipCode;
-let yourAge;
-let strReferralSource;
-let mayWeContactYou;
+// let firstName;
+// let strLastName;
+// let address;
+// let city;
+// let state;
+// let iZipCode;
+// let yourAge;
+// let strReferralSource;
+// let mayWeContactYou;
+
 
 // STEP 5
-address = '1234 Avenue Way';
-city = 'San Diego';
-state = '1234 Avenue Way';
+// address = '1234 Avenue Way';
+// city = 'San Diego';
+// state = '1234 Avenue Way';
 
-let address = '1234 Avenue Way';
-let city = 'San Diego';
-let state =' 1234 Avenue Way';
+// let address = '1234 Avenue Way';
+// let city = 'San Diego';
+// let state =' 1234 Avenue Way';
 
-let address = '1234 Avenue Way', city = 'San Diego', state =' 1234 Avenue Way';
+// let address = '1234 Avenue Way', city = 'San Diego', state =' 1234 Avenue Way';
+
+// STEP 6
+// let firstName = 'Marissa';
+// let age = '32';
+// console.log(firstName + ' is ' + age);
+
+
+// STEP 7
+// let boolString = 'Hello World!' + false;
+// alert(boolString);
+
+// let boolNumber = 12 + true;
+// alert(boolNumber);
+
+
+// STEP 8
+// let someString = "Who once said, 'Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.'";
+// console.log(someString);
+
+
+// STEP 9
+// let x = null;
+// let y;
+
+// console.log(x);
+// console.log(y);
+
+
+// STEP 10
+// console.log(typeof false);    // returns 'boolean'
+// console.log(typeof 2);        // returns 'number'
+// console.log(typeof 'Hello world!');   // returns 'string'
+// console.log(typeof {x:1, y:2}); // returns 'object'
+// console.log(typeof firstName); // returns 'undefined'
+
+
+// STEP 11
+// alert('Hello ' + 'Marissa Ade' + ', welcome to the JavaScript class!');
+
+
+// STEP 12
+// let name = 'Marissa Ade';
+// alert('Hello ' + name + ', welcome to the JavaScript class!');
+
+
+// STEP 13 
+// let name = 'Marissa Ade';
+// let course = 'Javascript';
+// alert('Hello ' + name + ', welcome to the ' + course + ' class!' );
+
+
+// STEP 14
+// let name = 'Marissa Ade';
+// let course = 'Javascript';
+// alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!' );
+
+
+// STEP 15
+// let name = prompt('What is your name?');
+// let course = 'Javascript';
+// alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!' );
+
+
+// STEP 16
+// let name = prompt('What is your name?');
+// let course = prompt('What class are you taking?');
+// alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!' );
+
+
+// STEP 17
+// let x = 10;
+// let y = 20;
+// console.log(x + y);
+
+
+// STEP 18
+// let x = 20;
+// console.log(x + 20);
+
+
+// STEP 19
+// let x = 20;
+// x *= 5;
+// console.log(x);
+
+
+// STEP 20 FIX THIS
+// let x = 20/3;
+// // console.log(x);
+// x /= 1;
+// console.log(x);
+
+
+// STEP 21
+// let a = 10;
+// let b = 20;
+// let c = 'Hello world!'
+
+// console.log(a == 10);
+
+
+// STEP 22
+// let a = 5;
+// let b = 8;
+// let c = 'Welcome'
+
+// console.log(c == 5);
